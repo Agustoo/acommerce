26d270494bfdb5dcf70bea140d46e2bdbc05728a
Agustoo <erenince2001@hotmail.com>
2022-12-23 18:36:49 +0300

    Header created

--- lib/home/main_food_page.dart
+++ lib/home/main_food_page.dart
@@ -1,7 +1,11 @@
+import 'package:acommerce/widgets/big_text.dart';
+import 'package:acommerce/widgets/small_text.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+
+import '../utils/colors.dart';
 
 class MainFoodPage extends StatefulWidget {
   const MainFoodPage({super.key});
@@ -13,26 +17,43 @@ class MainFoodPage extends StatefulWidget {
 class _MainFoodPageState extends State<MainFoodPage> {
   @override
   Widget build(BuildContext context) {
-    return Container( 
-      child: Container(
-        child: Row(
-          children: [
-            Column(
-              children: [
-                Text("Country"),
-                Text("City")
-              ],
-            ),
-            Container(
-              width: 45,
-              height: 45,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15),
-                color: Colors.blue,
+    return Scaffold(
+      body: Column(
+        children: [
+          Container( 
+            child: Container(
+              margin: EdgeInsets.only(top:45, bottom:15),
+              padding: EdgeInsets.only(left: 20, right: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: [
+                      BigText(text: "Turkey", color: AppColors.mainColor,),
+                      Row(
+                        children: [
+                          Padding(padding: EdgeInsets.only(left: 15)),
+                          SmallText(text: "Amasya", color: Colors.black54),
+                          Icon(Icons.arrow_drop_down_rounded)
+                      ],)
+                    ],
+                  ),
+                  Center(
+                    child: Container(
+                      width: 45,
+                      height: 45,
+                      child: Icon(Icons.search, color: Colors.white),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: AppColors.mainColor,
+                      ),
+                    ),
+                  )
+                ],
               ),
-            )
-          ],
-        ),
+            ),
+          ),
+        ],
       ),
     );
   }
