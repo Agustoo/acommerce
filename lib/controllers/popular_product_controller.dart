343e4f6aef84d9ee381598bb2360938cb674b90f
Agustoo <erenince2001@hotmail.com>
2023-01-01 16:59:19 +0300

    Transfer

--- lib/controllers/popular_product_controller.dart
+++ lib/controllers/popular_product_controller.dart
@@ -1,5 +1,7 @@
 import 'package:acommerce/data/repository/popular_product_repo.dart';
 import 'package:acommerce/models/products_model.dart';
+import 'package:acommerce/utils/colors.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class PopularProductController extends GetxController{
@@ -11,13 +13,16 @@ class PopularProductController extends GetxController{
   bool _isLoaded = false;
   bool get isLoaded=>_isLoaded;
 
+  int _quantity=0;
+  int get quantity=>_quantity;
+  int _inCartItems=0;
+  int get inCartItems=>_inCartItems+_quantity;
+
   Future<void> getPopularProductList()async{
     Response response = await popularProductRepo.getPopularProductList();
     if(response.statusCode==200){
-      print("got products");
       _popularProductList=[];
       _popularProductList.addAll(Product.fromJson(response.body).products);
-      //print(_popularProductList);
       _isLoaded=true;
       update();
 
@@ -26,4 +31,42 @@ class PopularProductController extends GetxController{
 
     }
   }
+
+  void setQuantity(bool isIncrement){
+    if(isIncrement){
+      _quantity=checkQuantity(_quantity+1);
+    }else{
+      _quantity=checkQuantity(_quantity-1);
+    }
+    update();
+  }
+  checkQuantity(int quantity){
+    if(quantity<0){
+      Get.snackbar("Item Count", "You can't reduce more!",
+      backgroundColor: AppColors.mainColor,
+      colorText: Colors.white,
+      );
+      return 0;
+    }else if(quantity>20){
+      Get.snackbar("Item Count", "You can't add more!",
+      backgroundColor: AppColors.mainColor,
+      colorText: Colors.white,
+      );
+      return 20;
+    }else{
+      return quantity;
+    }
+  }
+
+  void initProduct(){
+    _quantity=0;
+    _inCartItems=0;
+    //if exist
+    //get from storage _inCartItems=3
+  }
+
+  void addItem(ProductModel product){
+    
+  }
+
 }
