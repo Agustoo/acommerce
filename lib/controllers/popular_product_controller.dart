49272a19c435f87772d1ca0a60aa5caf42a97805
Eren İnce <erenince2001@hotmail.com>
2023-02-03 22:10:17 +0300

    CartController+CartRepo

--- lib/controllers/popular_product_controller.dart
+++ lib/controllers/popular_product_controller.dart
@@ -1,29 +1,77 @@
+import 'package:acommerce/controllers/cart_controller.dart';
 import 'package:acommerce/data/repository/popular_product_repo.dart';
 import 'package:acommerce/models/products_model.dart';
+import 'package:acommerce/utils/colors.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class PopularProductController extends GetxController{
+class PopularProductController extends GetxController {
   final PopularProductRepo popularProductRepo;
   PopularProductController({required this.popularProductRepo});
-  List<ProductModel> _popularProductList=[];
-  List<ProductModel> get popularProductList =>_popularProductList;
+  List<ProductModel> _popularProductList = [];
+  List<ProductModel> get popularProductList => _popularProductList;
+  late CartController _cart;
 
   bool _isLoaded = false;
-  bool get isLoaded=>_isLoaded;
+  bool get isLoaded => _isLoaded;
 
-  Future<void> getPopularProductList()async{
+  int _quantity = 0;
+  int get quantity => _quantity;
+  int _inCartItems = 0;
+  int get inCartItems => _inCartItems + _quantity;
+
+  Future<void> getPopularProductList() async {
     Response response = await popularProductRepo.getPopularProductList();
-    if(response.statusCode==200){
+    if (response.statusCode == 200) {
       print("got products");
-      _popularProductList=[];
+      _popularProductList = [];
       _popularProductList.addAll(Product.fromJson(response.body).products);
       //print(_popularProductList);
-      _isLoaded=true;
+      _isLoaded = true;
       update();
+    } else {}
+  }
 
-
-    }else{
-
+  void setQuantity(bool isIncrement) {
+    if (isIncrement) {
+      _quantity = checkQuantity(_quantity + 1);
+    } else {
+      _quantity = checkQuantity(_quantity - 1);
     }
+    update();
+  }
+
+  int checkQuantity(int quantity) {
+    if (quantity < 0) {
+      Get.snackbar(
+        "Item Count",
+        "You can't reduce more !",
+        backgroundColor: AppColors.mainColor,
+        colorText: Colors.white,
+      );
+      return 0;
+    } else if (quantity > 20) {
+      Get.snackbar(
+        "Item Count",
+        "You can't add more !",
+        backgroundColor: AppColors.mainColor,
+        colorText: Colors.white,
+      );
+      return 20;
+    } else {
+      return quantity;
+    }
+  }
+
+  void initProduct(CartController cart) {
+    _quantity = 0;
+    _inCartItems = 0;
+    _cart = cart;
+    //if exist
+    //get from storage _inCartItems
+  }
+
+  void addItem(ProductModel product) {
+    _cart.addItem(product, _quantity);
   }
 }
